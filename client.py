25e2d6559beff677afeeecc44a8cb0d620d82444
Lucien Christie-Dervaux <lucien@dark-oracle-36.dynamic2.rpi.edu>
2017-11-19 15:00:45 -0500

    adding more to client

--- client.py
+++ client.py
@@ -1,11 +1,14 @@
 class Proposer(Agent):
-    pass
+    def propose():
+        pass
 
 class Learner(Agent):
-    pass
+    def learn():
+        pass
 
 class Accepter(Agent):
-    pass
+    def accept():
+        pass
 
 class DistinguishedLearner(Learner):
     pass
@@ -20,28 +23,37 @@ class Client:
     #this class references the Log,
     #and encapsulates the proposer, learner and acceptor agents.
 
-    def tweet():
+    self.event_queue = []
 
+    def tweet(new_tweet):
+        self.event_queue.append(new_tweet)
         #add to queue of proposals you want accepted (ordered) (in memory)
 
-    def block():
-
+    def block(new_block):
+        self.event_queue.append(new_block)
         #add to queue of proposals you want accepted (ordered) (in memory)
 
-    def unblock():
-
+    def unblock(new_unblock):
+        self.event_queue.append(new_unblock)
         #add to queue of proposals you want accepted (ordered) (in_memory)
 
     #once your event reaches consensus among all other processes,
     #you can then add it to your own Log.
     def tweet_for_real(new_tweet):
         Log.tweet(new_tweet)
+        communicator.tweet()#just sends all messages that have not been recieved by the particular other clients.
+        #could be single tweet, single block, single unblock, as long as all the events
+        #in there have been the result of consensus. Which they are, as any event in the log has been reached by consensus!!!
 
     def block_for_real(new_block):
         Log.block(new_block)
+        communicator.tweet()#just sends all messages that have not been recieved by the particular other clients.
+        #could be single tweet, single block, single unblock
 
     def unblock_for_real(new_unblock):
         Log.unblock(new_unblock)
+        communicator.tweet()#just sends all messages that have not been recieved by the particular other clients.
+        #could be single tweet, single block, single unblock
 
     #these are to let you know if you are the leader.
     def isDistinguishedLearner():
